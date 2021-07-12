6cbbbff771966944c09ec30b2af1382fbf668f7a
panzerkovic <panzerkovic@gmail.com>
2021-07-12 05:54:56 -0300

    initial commit

---
+++ README.md
@@ -0,0 +1,21 @@
+## Introduction
+
+Car online store PoC
+
+
+## How to run
+
+I'm still configuring it all but you can run a dev mode using:
+
+```npm run dev```
+        or
+```npx webpack serve --mode development```
+
+if you are not affraid :)
+
+## TODO LIST:
+
+* Configure production build
+* Refact styled components into own dir
+* Unit testing
+
---
+++ src/app.js
@@ -0,0 +1,21 @@
+import React from "react";
+import { hot } from "react-hot-loader";
+import TodoList from "./todos/TodoList";
+import styled from "styled-components";
+
+const AppContainer = styled.div`
+  font-family: Arial, Helvetica, sans-serif;
+  color: #222222;
+  width: 100vw;
+  height: 100vh;
+  margin: 0;
+  padding: 0;
+`;
+
+const App = () => (
+  <AppContainer>
+    <TodoList />
+  </AppContainer>
+);
+
+export default hot(module)(App);
